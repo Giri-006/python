f9ca7aca483a681cb86feb151ba88b6d84afd3fe
Giri-006 <darknightfury16@gmail.com>
2026-07-29 18:40:21 +0530

    Initialize NLP.py with key concepts and metrics
    Add initial content for NLP concepts and metrics.

---
+++ NLP.py
@@ -0,0 +1,55 @@
+NLP --
+
+1. classification
+2. input text as input
+3. we predict 
+
+1500 -- Healthcare
+
+1. policy related documents
+2. claims related documents
+
+
+movie review --
+best dl or ml
+
+
+1500 -- Education system -- blog
+
+1. Education policy related documents
+2. New change in the educational symtem
+
+intent detection
+entity recognition
+labelling specific words
+
+1. question and answering
+2. sumarisation
+
+
+This toy is cute.
+
+1. token level
+2. entity level
+
+METRICS -- NLP
+BLEU
+ROUGUE
+
+PERPLEXITY AI - REFRENCE FREE METRICS
+
+
+REGULARISATION
+OPTIMIZATION
+FEED FORWARD NETWORKS
+CNN, RNN, 
+LSTM -- LSTM
+VANISHING GRADIENT
+AUTO ENCODERS --->
+INFERENCE 
+MONTE CARLO 
+METHOS
+PARTTION FUNCTIONS
+STRUCTURED PROBALISTIC MODEL
+LINEAR FACTOR MODELS
+DEEP GENERATIVE MODELS
