e2b556c1cfc8d7769ef0a94cfb6d00b182083306
Giri-006 <darknightfury16@gmail.com>
2026-07-29 19:14:31 +0530

    Add Python basics tutorial with data types and operators

---
+++ Python_Basics.py
@@ -0,0 +1,143 @@
+
+5
+
+# int, float, str, bool
+
+5
+
+10
+
+5
+
+# 5 int
+
+# int -->  number, bigint, smallint
+# float --> decimal
+# str --> character, char, varchar, varchar2
+# bool --> boolean
+
+5
+
+5.23456
+
+-5.123
+
+arun #NameError: name 'arun' is not defined
+
+# single quotes
+# double quotes
+
+'arun'
+
+"arun"
+
+# data -- data -- data --  5, 5.1234, 'arun'
+# a = 5 -- int
+# name = 'arun' -- str
+# variable, arguments, parameter -- type 
+
+true #NameError: name 'true' is not defined
+
+True
+
+False
+
+a = 10  # assignment operator =
+        # equality operator ==
+
+# depending on the value my type changes
+
+a = 10
+type(a)
+
+a = 5.12345
+type(a)
+
+a = 'Arun'
+type(a)
+
+a = True
+type(a)
+
+# depeding on the data the type fo variable is changing
+
+# Operators
+# Arithmetic Operator
+# Relationald/Comparison Operator
+# Logical Operator
+
+# AO
+
+# +, - , *, /, //(double division), %(modulus)
+
+5 + 5 # Returned value --> int
+
+5 - 5 # int
+
+5 * 5 # int
+
+5 / 5 # float
+
+5 // 5 # int
+
+5 % 5 
+
+a = 5
+b = 5
+c = a % b
+
+c
+
+a, b = 5, 5
+c = a % b
+print(c)
+
+# Comparison operator , > , >=, < , <= , !=, ==
+
+a = 10
+b = 20
+b >= a
+
+a == b
+
+a != b
+
+a = 'arun'
+b = 'balaji'
+a > b
+
+# Joseph Vijay # CM
+# Giri Raj # sequence of characters
+
+a == b
+
+a = True
+b = True
+a == b
+
+# all these comparison operation, the primary usage will be in conditional statement
+
+# LO
+
+# AND, OR, NOT
+
+c = a + b # 2 operands
+c = a and b # 2 operands
+
+# truth table
+# 1 - True
+# 0 - False
+#       and or   not  
+#  a b  c    c    a  b  
+# 0 0  0    0    1  1
+# 0 1  0    1    1  0
+# 1 0  0    1    0  1
+# 1 1  1    1    0  0
+
+a = 10
+
+a > 5 and a < 15  # and
+
+a > 5 or a < 2 # or
+
+not(a > 5 and a < 15)
